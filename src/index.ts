2673f2f28c0333d7f6de657493e69a2b2e6ba420
Guilherme Brunow <gbrunow@outlook.com>
2019-01-15 11:12:42 -0600

    some progress

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('test')
+import { Effect } from './effect';
+import { State, StateArgs } from './state';
+
+const escape: Effect = {
+    event: 'escape',
+    action: (state: StateArgs) => console.log(`escape ${state.name}`)
+}
+
+const initial = State({ name: 'initial' })
+    .addEffect({
+        event: 'enter',
+        action: (state: StateArgs) => console.log(`entered ${state.name}`)
+    });
+// .addEffect({
+//     event: 'leave',
+//     action: (state: StateArgs) => console.log(`leave ${state.name}`)
+// })
+// .addEffect(escape);
+
+// initial.effects[0].action({ name: 'initial' });
+console.log(initial);
+console.log('end');
