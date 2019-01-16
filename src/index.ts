4ff0eb5964d4388ecc51f37fe95fa57c32ff8338
Guilherme Brunow <gbrunow@outlook.com>
2019-01-16 14:01:20 -0600

    refactoring state to use 'class' :disappointed: object composition is cool and all but this is a very small library and the clock is ticking

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,55 @@
 import { Reaction } from './reaction';
-import { CreateState, State, StateProperties } from './state';
+import { State } from './state';
 
 const escape: Reaction = {
     event: 'escape',
-    action: (state: StateProperties) => console.log(` > escaping ${state.name} state`)
+    action: (state: State) => console.log(` > escaping ${state.name} state`)
 }
 
 const click: Reaction = {
     event: 'click',
-    action: (state: StateProperties) => console.log(` > reacting to click on ${state.name} state`)
+    action: (state: State) => console.log(` > reacting to click on ${state.name} state`)
 }
 
-const initial: State = CreateState({ name: 'initial' })
+const initial: State = (new State({ name: 'initial' }))
     .add({
         event: 'enter',
-        action: (state: StateProperties) => {
-            console.log(` > entering ${state.name} state`);
-        }
+        action: (state: State) => console.log(` > entering ${state.name} state`)
     })
     .add({
         event: 'enter',
-        action: (state: StateProperties) => console.log(` > tell someone else we're entering ${state.name} state`)
+        action: (state: State) => console.log(` > tell someone else we're entering ${state.name} state`)
     })
     .add({
         event: 'leave',
-        action: (state: StateProperties) => console.log(` > leaving ${state.name} state`)
+        action: (state: State) => console.log(` > leaving ${state.name} state`)
     })
     .add(click)
     .add(escape);
 
-const sayHeyOnEnter: State = CreateState({
-    name: 'talker', reactions: [{
-        event: 'enter',
-        action: (state: StateProperties, data: { getnum: () => number }) => console.log(` + ${state.name} says: hey.${".".repeat(data.getnum())}`)
-    }]
-}).add(click, escape);
+initial.emit('enter');
+
+const sayHeyOnEnter: State = (new State(
+    {
+        name: 'talker',
+        reactions: [
+            {
+                event: 'enter',
+                action: (state: State, data: { getnum: () => number }) => console.log(` + ${state.name} says: hey.${".".repeat(data.getnum())}`)
+            }
+        ]
+    }))
+    .add(click, escape);
 
 const sayByeOnLeave = sayHeyOnEnter
     .extend({ name: 'another talker' })
     .add(
         {
             event: 'leave',
-            action: (state: StateProperties, data: { person: string }) => console.log(` - ${state.name} says: you know nothing, ${data.person}.`)
+            action: (state: State, data: { person: string }) => console.log(` - ${state.name} says: you know nothing, ${data.person}.`)
         }, {
             event: 'click',
-            action: (state: StateProperties) => console.log(` - another click handler on ${state.name}`)
+            action: (state: State) => console.log(` - another click handler on ${state.name}`)
         }
     )
     .emit('enter', { getnum: () => Math.random() * 10 })
@@ -52,7 +57,6 @@ const sayByeOnLeave = sayHeyOnEnter
     .emit('escape')
     .emit('leave', { person: 'jon snow' });
 
-initial.emit('enter');
 
 sayHeyOnEnter
     .emit('enter', { getnum: () => Math.random() * 25 })
--- src/reaction.ts
+++ src/reaction.ts
@@ -1,9 +1,9 @@
-import { StateProperties } from './state';
+import { State } from './state';
 
 /**
  * Actions are callbacks used by reactions
  */
-export type Action = (state: StateProperties, data?: any) => void;
+export type Action = (state: State, data?: any) => void;
 
 /**
  * Reaction defines side effects to a given event
@@ -12,10 +12,3 @@ export type Reaction = {
     event: string; // enter, leave, escape, click, etc
     action: Action;
 }
-
-export const canReact = (obj: any) => ({
-    add: (...reactions: Reaction[]) => {
-        obj.reactions.push(...reactions);
-        return obj;
-    }
-});
